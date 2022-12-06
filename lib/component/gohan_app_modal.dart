eda001ce789b5ee644edd0de2e1f6e77a1ad5524
Shingo TOYODA <toyodasinngo@gmail.com>
2022-12-07 01:24:42 +0900

    メイン画面と登録画面の遷移を作成

---
+++ lib/component/gohan_app_modal.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/Cupertino.dart';
+import 'package:flutter/material.dart';
+
+class GohanAppModal extends StatelessWidget {
+  final double? height;
+  final Widget child;
+  const GohanAppModal({
+    required this.child,
+    this.height,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: height,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.3),
+              spreadRadius: 5,
+              blurRadius: 10,
+              offset: const Offset(0, 0), // changes position of shadow
+            ),
+          ],
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(10.0),
+            topRight: Radius.circular(10.0),
+          ),
+        ),
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.fromLTRB(0, 16, 0, 15),
+              height: 5,
+              width: 26,
+              decoration: BoxDecoration(
+                color: const Color(0x26000000),
+                borderRadius: BorderRadius.circular(2.5),
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: child,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
