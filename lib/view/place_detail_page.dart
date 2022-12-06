ffd93ed54c219092e182324d723cafac99eec940
Shingo TOYODA <toyodasinngo@gmail.com>
2022-12-07 01:42:31 +0900

    投稿画面の遷移を作成

--- lib/view/place_detail_page.dart
+++ lib/view/place_detail_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/Cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gohan_map/component/gohan_app_modal.dart';
+import 'package:gohan_map/view/place_post_page.dart';
 
 class PlaceDetailPage extends StatelessWidget {
   const PlaceDetailPage({Key? key}) : super(key: key);
@@ -14,10 +15,29 @@ class PlaceDetailPage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text('Detail'),
+            const Text('PlaceDetailPage'),
+            CupertinoButton(
+              child: const Text('Post'),
+              onPressed: () {
+                showModalBottomSheet(
+                  barrierColor: Colors.black.withOpacity(0),
+                  context: context,
+                  isScrollControlled: true,
+                  builder: (context) {
+                    return const PlacePostPage();
+                  },
+                );
+              },
+            ),
+            CupertinoButton(
+              child: const Text('Delete'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
             CupertinoButton(
               child: const Text('Close'),
-              onPressed: () {
+              onPressed: () {  
                 Navigator.pop(context);
               },
             ),
