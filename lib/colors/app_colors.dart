631d4ce646fcc4ff09ead32b966479b21d75e3c0
Shingo TOYODA <toyodasinngo@gmail.com>
2022-12-13 11:20:39 +0900

    App専用に色を定義

---
+++ lib/colors/app_colors.dart
@@ -0,0 +1,10 @@
+
+import 'package:flutter/Cupertino.dart';
+
+class AppColors {
+  // static const Color primaryColor = Color(0xFF1E1E1E);
+  // static const Color secondaryColor = Color(0xFF1E1E1E);
+  static const Color searchBarColorColor = Color(0xFFC1C1C1);
+  static const Color backgroundWhiteColor = Color(0xFFFFFFFF);
+  static const Color backgroundGrayColor = Color(0xFFD9D9D9);
+}
