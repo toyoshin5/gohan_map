edc97c5619f0654e4f214bd87c978af49ce28744
Shingo TOYODA <toyodasinngo@gmail.com>
2022-12-13 15:02:53 +0900

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/Cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:gohan_map/colors/app_colors.dart';
 import 'package:gohan_map/view/map_page.dart';
 
 /// アプリが起動したときに呼ばれる
@@ -17,22 +18,25 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const CupertinoApp(
+    return CupertinoApp(
       title: 'Gohan Map',
       //ステータスバーの文字を消す
       debugShowCheckedModeBanner: false,
       home: CupertinoPageScaffold(
         resizeToAvoidBottomInset: false,//キーボードが出てきたときに画面を上にずらすかどうか
         //Scaffoldは画面の枠組みを作るウィジェット。基本的アプリに1つで、画面全体を覆う最上位ウィジェットであることを示す。
-        navigationBar: CupertinoNavigationBar(
+        navigationBar: const CupertinoNavigationBar(
           //上のバー
           middle: Text('Gohan Map'), //上のバーの中央に表示される文字
         ),
-        child: SafeArea(
-          child: MapPage(),//最初の画面
+        child: Container(
+          color: AppColors.backgroundGrayColor,
+          child: const SafeArea(
+            child: MapPage(),//最初の画面
+          ),
         ),
       ),
-      localizationsDelegates: [
+      localizationsDelegates: const [
               DefaultMaterialLocalizations.delegate,
               DefaultCupertinoLocalizations.delegate,
               DefaultWidgetsLocalizations.delegate,
