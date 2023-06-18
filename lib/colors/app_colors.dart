9be629003131b323550b1b2bdea8f2c77f28266a
karintou8710 <karintou8710@gmail.com>
2023-06-18 13:02:32 +0900

    [add] timeline button

--- lib/colors/app_colors.dart
+++ lib/colors/app_colors.dart
@@ -1,10 +1,11 @@
-
 import 'package:flutter/Cupertino.dart';
 
 class AppColors {
   // static const Color primaryColor = Color(0xFF1E1E1E);
   // static const Color secondaryColor = Color(0xFF1E1E1E);
   static const Color blackTextColor = Color(0xFF000000);
+  static const Color redTextColor = Color(0xFFDF3030);
+  static const Color blueTextColor = Color(0xFF2F1F8E);
   static const Color searchBarColorColor = Color(0xFFC1C1C1);
   static const Color backgroundWhiteColor = Color(0xffffffff);
   static const Color backgroundGrayColor = Color(0xffd9d9d9);
--- lib/view/place_post_page.dart
+++ lib/view/place_post_page.dart
@@ -1,5 +1,9 @@
+import 'dart:ui';
+
 import 'package:flutter/Cupertino.dart';
+import 'package:gohan_map/colors/app_colors.dart';
 import 'package:gohan_map/component/app_modal.dart';
+
 /// 飲食店でのごはん投稿画面
 class PlacePostPage extends StatelessWidget {
   const PlacePostPage({Key? key}) : super(key: key);
@@ -7,26 +11,54 @@ class PlacePostPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AppModal(
-      initialChildSize: 0.5,
+      initialChildSize: 0.7,
       minChildSize: 0.5,
-      maxChildSize: 0.5,
-      child: Padding(//余白を作るためのウィジェット
+      maxChildSize: 0.7,
+      child: Padding(
+        //余白を作るためのウィジェット
         padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(//縦に並べるためのウィジェット
+        child: Column(
+          //縦に並べるためのウィジェット
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text('PlacePostPage'),
-            CupertinoButton(
-              child: const Text('Post'),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
+            const Text('トリトン'),
+            const Text("北海道札幌市豊平区豊平４条６丁目１−１０"),
+            Container(
+                width: double.infinity,
+                height: 50,
+                margin: const EdgeInsets.only(bottom: 10),
+                child: CupertinoButton(
+                  color: AppColors.backgroundWhiteColor,
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text(
+                    '決定',
+                    style: TextStyle(
+                        color: AppColors.blueTextColor,
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold),
+                  ),
+                )),
+            Container(
+                width: double.infinity,
+                height: 50,
+                child: CupertinoButton(
+                  color: AppColors.backgroundWhiteColor,
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text(
+                    'キャンセル',
+                    style: TextStyle(
+                        color: AppColors.redTextColor,
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold),
+                  ),
+                )),
           ],
-        ),  
+        ),
       ),
     );
   }
 }
-
-
