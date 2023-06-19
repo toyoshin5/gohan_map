668c3566cfa0d4bb7bee0807cdbecdfd8c60123b
AlMikan <nekomimi987@gmail.com>
2023-06-19 20:47:33 +0900

    add isar db

---
+++ lib/collections/shop.dart
@@ -0,0 +1,18 @@
+import 'dart:ffi';
+
+import 'package:isar/isar.dart';
+
+part 'shop.g.dart';
+
+@Collection()
+class Shop {
+  Id id = Isar.autoIncrement;
+
+  late String shopName;
+  late String shopAddress;
+  late double shopLatitude;
+  late double shopLongitude;
+  late int shopStar;
+  late DateTime createdAt;
+  late DateTime updatedAt;
+}
