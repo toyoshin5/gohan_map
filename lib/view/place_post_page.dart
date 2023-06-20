7bcccb63b1278a169c0a8a39d8b15d667d4677ce
karintou8710 <karintou8710@gmail.com>
2023-06-20 23:11:17 +0900

    [add] date picker

--- lib/view/place_post_page.dart
+++ lib/view/place_post_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/Cupertino.dart';
 import 'package:gohan_map/colors/app_colors.dart';
 import 'package:gohan_map/component/app_modal.dart';
 import 'package:flutter/material.dart';
+import 'package:date_time_picker/date_time_picker.dart';
 
 /// 飲食店でのごはん投稿画面
 class PlacePostPage extends StatelessWidget {
@@ -32,23 +33,41 @@ class PlacePostPage extends StatelessWidget {
               ),
               padding: const EdgeInsets.all(24),
               margin: const EdgeInsets.symmetric(vertical: 45),
-              child: TextField(
-                keyboardType: TextInputType.multiline,
-                maxLines: 4,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: const BorderSide(
-                        width: 0,
-                        style: BorderStyle.none,
-                      ),
+              child: Column(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.symmetric(vertical: 15),
+                    child: DateTimePicker(
+                      dateMask: 'yyyy/MM/dd',
+                      initialValue: DateTime.now().toString(),
+                      firstDate: DateTime(2000),
+                      lastDate: DateTime(2100),
+                      decoration: const InputDecoration(
+                          icon: Icon(Icons.event),
+                          filled: true,
+                          labelText: '訪問日'),
+                      onSaved: (val) => print(val),
                     ),
-                    hintText: 'コメント',
-                    contentPadding: const EdgeInsets.symmetric(
-                      vertical: 10,
-                      horizontal: 17,
-                    ),
-                    filled: true),
+                  ),
+                  TextField(
+                    keyboardType: TextInputType.multiline,
+                    maxLines: 4,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: const BorderSide(
+                            width: 0,
+                            style: BorderStyle.none,
+                          ),
+                        ),
+                        hintText: 'コメント',
+                        contentPadding: const EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 17,
+                        ),
+                        filled: true),
+                  ),
+                ],
               ),
             ),
             Container(
