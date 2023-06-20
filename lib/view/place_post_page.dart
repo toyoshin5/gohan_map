f467886d6d36b9d6931a1e7ccffac5ccc7586ee7
karintou8710 <karintou8710@gmail.com>
2023-06-20 22:42:20 +0900

    [add] comment input

--- lib/view/place_post_page.dart
+++ lib/view/place_post_page.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:flutter/Cupertino.dart';
 import 'package:gohan_map/colors/app_colors.dart';
 import 'package:gohan_map/component/app_modal.dart';
+import 'package:flutter/material.dart';
 
 /// 飲食店でのごはん投稿画面
 class PlacePostPage extends StatelessWidget {
@@ -23,6 +24,33 @@ class PlacePostPage extends StatelessWidget {
           children: [
             const Text('トリトン'),
             const Text("北海道札幌市豊平区豊平４条６丁目１−１０"),
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: AppColors.backgroundWhiteColor,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              padding: const EdgeInsets.all(24),
+              margin: const EdgeInsets.symmetric(vertical: 45),
+              child: TextField(
+                keyboardType: TextInputType.multiline,
+                maxLines: 4,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(
+                        width: 0,
+                        style: BorderStyle.none,
+                      ),
+                    ),
+                    hintText: 'コメント',
+                    contentPadding: const EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 17,
+                    ),
+                    filled: true),
+              ),
+            ),
             Container(
                 width: double.infinity,
                 height: 50,
