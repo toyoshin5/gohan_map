109a66b2ba3ea40d7d4a1f6e54e3d1be8fb12241
Shingo TOYODA <toyodasinngo@gmail.com>
2023-07-09 17:55:21 +0900

    タイムラインから飲食店編集への遷移を作成

--- lib/view/place_detail_page.dart
+++ lib/view/place_detail_page.dart
@@ -4,6 +4,7 @@ import 'package:gohan_map/collections/shop.dart';
 import 'package:gohan_map/collections/timeline.dart';
 import 'package:gohan_map/component/app_modal.dart';
 import 'package:gohan_map/view/place_post_page.dart';
+import 'package:gohan_map/view/place_update_page.dart';
 
 ///飲食店の詳細画面
 class PlaceDetailPage extends StatelessWidget {
@@ -15,8 +16,8 @@ class PlaceDetailPage extends StatelessWidget {
     final Shop dummyShop = Shop()
       ..id = 1
       ..shopAddress = "北海道札幌市豊平区平岸２条１５丁目４−２３"
-      ..shopLatitude = 30
-      ..shopLongitude = 145
+      ..shopLatitude = 43.024318
+      ..shopLongitude = 141.366267
       ..shopName = "トリトン"
       ..shopStar = 3
       ..createdAt = DateTime.now()
@@ -69,6 +70,23 @@ class PlaceDetailPage extends StatelessWidget {
                     return PlacePostPage(
                       shop: dummyShop,
                       timeline: dummyTimeline,
+                    ); //ご飯投稿
+                  },
+                );
+              },
+            ),
+             CupertinoButton(
+              child: const Text('EditShop'),
+              onPressed: () {
+                showModalBottomSheet(
+                  //モーダルを表示する関数
+                  barrierColor: Colors.black.withOpacity(0), //背景をどれぐらい暗くするか
+                  backgroundColor: Colors.transparent,
+                  context: context,
+                  isScrollControlled: true, //スクロールで閉じたりするか
+                  builder: (context) {
+                    return PlaceUpdatePage(
+                      shop: dummyShop,
                     ); //ご飯投稿
                   },
                 );
