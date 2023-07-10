2d5781b640b178ebf48567e40d834de8b87ca93b
karintou8710 <karintou8710@gmail.com>
2023-07-10 20:31:01 +0900

    [add] support present location streaming

--- lib/component/app_map.dart
+++ lib/component/app_map.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:gohan_map/colors/app_colors.dart';
@@ -22,116 +24,119 @@ class AppMap extends StatefulWidget {
 }
 
 class _AppMapState extends State<AppMap> with TickerProviderStateMixin {
-  late Future<Position> future;
   Marker? presetLocationMarker;
-  late LatLng currentPosition;
+  LatLng? currentPosition;
+  late StreamSubscription<Position> positionStream;
   bool isCurrentLocation = true;
+
+  final LocationSettings locationSettings = const LocationSettings(
+    accuracy: LocationAccuracy.high,
+    distanceFilter: 1,
+  );
 
   @override
   void initState() {
     super.initState();
-    future = Future(() async {
-      var pos = await getCurrentPosition();
-      setPresetLocationMarker(LatLng(pos.latitude, pos.longitude));
-      return pos;
-    });
+    init();
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<Position>(
-      future: future,
-      builder: (context, snapshot) {
-        switch (snapshot.connectionState) {
-          case ConnectionState.none:
-            return const SizedBox();
-          case ConnectionState.waiting:
-            return const SizedBox();
-          case ConnectionState.active:
-            return const SizedBox();
-          case ConnectionState.done:
-            currentPosition =
-                LatLng(snapshot.data!.latitude, snapshot.data!.longitude);
-            return Stack(
-              children: [
-                FlutterMap(
-                  options: MapOptions(
-                    center: currentPosition,
-                    minZoom: 3,
-                    maxZoom: 18,
-                    zoom: 13,
-                    interactiveFlags: InteractiveFlag.all,
-                    enableMultiFingerGestureRace: true,
-                    onLongPress: widget.onLongPress,
-                    onPositionChanged: (position, hasGesture) {
-                      if (hasGesture && isCurrentLocation) {
-                        setState(() {
-                          isCurrentLocation = false;
-                        });
-                      }
-                    },
-                  ),
-                  mapController: widget.mapController,
-                  nonRotatedChildren: [
-                    RichAttributionWidget(
-                      attributions: [
-                        TextSourceAttribution(
-                          'OpenStreetMap contributors',
-                          onTap: () => launchUrl(
-                              Uri.parse('https://openstreetmap.org/copyright')),
-                        ),
-                      ],
-                    ),
-                  ],
-                  children: [
-                    TileLayer(
-                      urlTemplate:
-                          'https://api.maptiler.com/maps/jp-mierune-streets/256/{z}/{x}/{y}@2x.png?key=j4Xnfvwl9nEzUVlzCdBr',
-                    ),
-                    if (widget.pins != null)
-                      MarkerLayer(
-                        markers: [...widget.pins!, presetLocationMarker!],
-                        rotate: true,
-                      ),
-                  ],
-                ),
-                //現在地に戻るボタン
-                Positioned(
-                  bottom: 120,
-                  right: 20,
-                  child: SizedBox(
-                    width: 44,
-                    height: 44,
-                    child: ElevatedButton(
-                      //角丸で白
-                      style: ElevatedButton.styleFrom(
-                        padding: const EdgeInsets.all(0),
-                        shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10)),
-                        ),
-                        backgroundColor: Colors.white,
-                        foregroundColor: AppColors.blueTextColor,
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          isCurrentLocation = true;
-                        });
-                        _animatedMapMove(currentPosition, 13);
-                      },
-                      child: Icon((isCurrentLocation)
-                          ? Icons.near_me
-                          : Icons.near_me_outlined),
-                    ),
-                  ),
+    if (currentPosition == null) {
+      return const SizedBox();
+    }
+
+    return Stack(
+      children: [
+        FlutterMap(
+          options: MapOptions(
+            center: currentPosition,
+            minZoom: 3,
+            maxZoom: 18,
+            zoom: 13,
+            interactiveFlags: InteractiveFlag.all,
+            enableMultiFingerGestureRace: true,
+            onLongPress: widget.onLongPress,
+            onPositionChanged: (position, hasGesture) {
+              if (hasGesture && isCurrentLocation) {
+                setState(() {
+                  isCurrentLocation = false;
+                });
+              }
+            },
+          ),
+          mapController: widget.mapController,
+          nonRotatedChildren: [
+            RichAttributionWidget(
+              attributions: [
+                TextSourceAttribution(
+                  'OpenStreetMap contributors',
+                  onTap: () => launchUrl(
+                      Uri.parse('https://openstreetmap.org/copyright')),
                 ),
               ],
-            );
-        }
-      },
+            ),
+          ],
+          children: [
+            TileLayer(
+              urlTemplate:
+                  'https://api.maptiler.com/maps/jp-mierune-streets/256/{z}/{x}/{y}@2x.png?key=j4Xnfvwl9nEzUVlzCdBr',
+            ),
+            if (widget.pins != null)
+              MarkerLayer(
+                markers: [...widget.pins!, presetLocationMarker!],
+                rotate: true,
+              ),
+          ],
+        ),
+        //現在地に戻るボタン
+        Positioned(
+          bottom: 120,
+          right: 20,
+          child: SizedBox(
+            width: 44,
+            height: 44,
+            child: ElevatedButton(
+              //角丸で白
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.all(0),
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+                backgroundColor: Colors.white,
+                foregroundColor: AppColors.blueTextColor,
+              ),
+              onPressed: () {
+                setState(() {
+                  isCurrentLocation = true;
+                });
+                _animatedMapMove(currentPosition!, 13);
+              },
+              child: Icon(
+                  (isCurrentLocation) ? Icons.near_me : Icons.near_me_outlined),
+            ),
+          ),
+        ),
+      ],
     );
   }
 
-  Future<Position> getCurrentPosition() async {
+  Future<void> init() async {
+    await checkGPSPermission();
+
+    // ユーザの現在位置を取得し続ける
+    positionStream =
+        Geolocator.getPositionStream(locationSettings: locationSettings)
+            .listen((Position position) {
+      var latLng = LatLng(position.latitude, position.longitude);
+      setPresetLocationMarker(latLng);
+      setState(() {
+        currentPosition = latLng;
+      });
+    });
+  }
+
+  Future<void> checkGPSPermission() async {
     // 位置情報サービスが有効かチェック
     var serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
@@ -151,14 +156,10 @@ class _AppMapState extends State<AppMap> with TickerProviderStateMixin {
       return Future.error(
           'Location permissions are permanently denied, we cannot request permissions.');
     }
-
-    var position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
-    return position;
   }
 
   void setPresetLocationMarker(LatLng latlng) {
-    const markerSize = 30.0;
+    const markerSize = 24.0;
 
     var marker = Marker(
         width: markerSize,
@@ -173,11 +174,11 @@ class _AppMapState extends State<AppMap> with TickerProviderStateMixin {
                 ],
               ),
               child: CircleAvatar(
-                radius: 30,
+                radius: 24,
                 backgroundColor: Colors.white,
                 child: Icon(
                   Icons.circle,
-                  size: 27,
+                  size: 20,
                   color: Colors.blue.shade600,
                 ),
               ),
