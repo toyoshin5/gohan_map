3625ea03ad1f751c9e80afefdaa6bc93cb3fa829
Shingo TOYODA <toyodasinngo@gmail.com>
2023-10-01 21:00:16 +0900

    1つ分の投稿をコンポーネント化

--- lib/view/place_detail_page.dart
+++ lib/view/place_detail_page.dart
@@ -1,18 +1,16 @@
-import 'dart:io';
 
-import 'package:flutter/Cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gohan_map/collections/shop.dart';
 import 'package:gohan_map/collections/timeline.dart';
 import 'package:gohan_map/colors/app_colors.dart';
 import 'package:gohan_map/component/app_modal.dart';
+import 'package:gohan_map/component/post_card_widget.dart';
 import 'package:gohan_map/utils/common.dart';
 import 'package:gohan_map/view/place_post_page.dart';
 import 'package:gohan_map/view/place_update_page.dart';
-import 'package:intl/intl.dart';
+
 import 'package:isar/isar.dart';
-import 'package:pull_down_button/pull_down_button.dart';
-import 'package:path/path.dart' as p;
+
 
 import '../component/app_rating_bar.dart';
 import '../utils/isar_utils.dart';
@@ -239,149 +237,43 @@ class _PlaceDetailPageState extends State<PlaceDetailPage> {
                 } else {
                   return Column(children: [
                     for (Timeline timeline in (shopTimeline ?? []))
-                      Column(children: [
-                        Card(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(16),
-                            ),
-                            elevation: 0,
-                            child: Row(
-                              children: [
-                                Expanded(
-                                  child: Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 8),
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.spaceBetween,
-                                          children: [
-                                            Row(
-                                              children: [
-                                                const Padding(
-                                                  padding:
-                                                      EdgeInsets.only(right: 6),
-                                                  child: Icon(
-                                                    Icons.access_time,
-                                                    size: 18,
-                                                  ),
-                                                ),
-                                                // アイコンとテキストの間のスペースを設定
-                                                Text(
-                                                  DateFormat('yyyy/MM/dd')
-                                                      .format(timeline.date),
-                                                  style: const TextStyle(
-                                                      fontSize: 14,
-                                                      color: Colors.black38),
-                                                ),
-                                                if (timeline.umai)
-                                                  const Padding(
-                                                    padding: EdgeInsets.only(
-                                                        left: 12),
-                                                    child: Icon(
-                                                      Icons.thumb_up,
-                                                      size: 18,
-                                                      color: Color(0xFF2196F3),
-                                                    ),
-                                                  ),
-                                              ],
-                                            ),
-                                            PullDownButton(
-                                              itemBuilder: (context) => [
-                                                PullDownMenuItem(
-                                                  onTap: () {
-                                                    if (selectedShop == null) {
-                                                      return;
-                                                    }
-                                                    showModalBottomSheet(
-                                                      //モーダルを表示する関数
-                                                      barrierColor: Colors.black
-                                                          .withOpacity(
-                                                              0), //背景をどれぐらい暗くするか
-                                                      backgroundColor:
-                                                          Colors.transparent,
-                                                      context: context,
-                                                      isScrollControlled:
-                                                          true, //スクロールで閉じたりするか
-                                                      builder: (context) {
-                                                        return PlacePostPage(
-                                                          shop: selectedShop!,
-                                                          timeline: timeline,
-                                                        ); //ご飯投稿
-                                                      },
-                                                    ).then((value) {
-                                                      IsarUtils
-                                                              .getTimelinesByShopId(
-                                                                  selectedShop!
-                                                                      .id)
-                                                          .then((timeline) {
-                                                        setState(() {
-                                                          shopTimeline =
-                                                              timeline;
-                                                        });
-                                                      });
-                                                    });
-                                                  },
-                                                  title: '編集',
-                                                  icon: CupertinoIcons.pencil,
-                                                ),
-                                                PullDownMenuItem(
-                                                  onTap: () {
-                                                    IsarUtils.deleteTimeline(
-                                                        timeline.id);
-                                                    setState(() {
-                                                      shopTimeline!
-                                                          .remove(timeline);
-                                                    });
-                                                  },
-                                                  title: '削除',
-                                                  isDestructive: true,
-                                                  icon: CupertinoIcons.delete,
-                                                ),
-                                              ],
-                                              animationBuilder: null,
-                                              position: PullDownMenuPosition
-                                                  .automatic,
-                                              buttonBuilder: (_, showMenu) =>
-                                                  CupertinoButton(
-                                                onPressed: showMenu,
-                                                padding: EdgeInsets.zero,
-                                                pressedOpacity: 1,
-                                                child: const Icon(
-                                                  CupertinoIcons.ellipsis,
-                                                  color: Colors.black,
-                                                  size: 24,
-                                                ),
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-                                        if (timeline.comment != "")
-                                          Padding(
-                                            padding: const EdgeInsets.only(
-                                                bottom: 8),
-                                            child: Text(
-                                              timeline.comment,
-                                              style:
-                                                  const TextStyle(fontSize: 15),
-                                            ),
-                                          )
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            )),
-                        if (timeline.image != null &&
-                            timeline.image!.isNotEmpty)
-                          Image.file(
-                            File(p.join(snapshot.data!, timeline.image!)),
-                            fit: BoxFit.fitWidth,
-                          )
-                      ])
+                      PostCard(
+                        timeline: timeline,
+                        selectedShop: selectedShop,
+                        snapshot: snapshot,
+                        onEditTapped: () {
+                          if (selectedShop == null) {
+                            return;
+                          }
+                          showModalBottomSheet(
+                            //モーダルを表示する関数
+                            barrierColor:
+                                Colors.black.withOpacity(0), //背景をどれぐらい暗くするか
+                            backgroundColor: Colors.transparent,
+                            context: context,
+                            isScrollControlled: true, //スクロールで閉じたりするか
+                            builder: (context) {
+                              return PlacePostPage(
+                                shop: selectedShop!,
+                                timeline: timeline,
+                              ); //ご飯投稿
+                            },
+                          ).then((value) {
+                            IsarUtils.getTimelinesByShopId(selectedShop!.id)
+                                .then((timeline) {
+                              setState(() {
+                                shopTimeline = timeline;
+                              });
+                            });
+                          });
+                        },
+                        onDeleteTapped: () {
+                          IsarUtils.deleteTimeline(timeline.id);
+                          setState(() {
+                            shopTimeline!.remove(timeline);
+                          });
+                        },
+                      )
                   ]);
                 }
               }),
@@ -393,3 +285,4 @@ class _PlaceDetailPageState extends State<PlaceDetailPage> {
         ]));
   }
 }
+
