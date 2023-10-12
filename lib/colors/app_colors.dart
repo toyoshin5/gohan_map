3dc7f370fd909617d5438c8f353bd1541a818747
Shingo TOYODA <toyodasinngo@gmail.com>
2023-10-13 01:12:22 +0900

    色を修正

--- lib/colors/app_colors.dart
+++ lib/colors/app_colors.dart
@@ -4,8 +4,8 @@ class AppColors {
   // static const Color primaryColor = Color(0xFF1E1E1E);
   // static const Color secondaryColor = Color(0xFF1E1E1E);
   static const Color blackTextColor = Color(0xFF000000);
-  static const Color primaryColor = Color(0xFFED9161);
-  static const Color tabBarColor = Color(0xFFE77745);
+  static const Color primaryColor = Color(0xFFFC8B56);
+  static const Color tabBarColor = Color(0xFFF86F35);
   static const Color redTextColor = Color(0xFFDF3030);
   static const Color searchBarColor = Color(0xFFE2DFDC);
   static const Color whiteColor = Color(0xffffffff);
