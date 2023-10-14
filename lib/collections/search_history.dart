b54dd6fa5e04c76e85033ca955ff74d93908f9e4
Shingo TOYODA <toyodasinngo@gmail.com>
2023-10-14 14:47:37 +0900

    DBを更新

--- lib/collections/search_history.dart
+++ lib/collections/search_history.dart
@@ -1,16 +1,13 @@
 import 'package:isar/isar.dart';
 
-part 'shop.g.dart';
+part 'search_history.g.dart';
 
 @Collection()
-class Shop {
+class SearchHistory {
   Id id = Isar.autoIncrement;
-
-  late String googlePlaceId;
-  late String shopName;
-  late String shopAddress;
-  late String shopMapIconKind;
-  late bool wantToGoFlg;
+  late String name;
+  late String placeId;
+  late String address;
   late double shopLatitude;
   late double shopLongitude;
   late DateTime createdAt;
